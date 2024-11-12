2ecb0e98c49e347f2671f007560a5b4a60e200d0
zxeni1 <zacxfitness@gmail.com>
2024-11-12 19:41:04 +1000

    CRA

---
+++ README.md
@@ -0,0 +1,5 @@
+# Term 3 Week 5 - Intro to React 
+
+- Tuesday:
+    - CRA (Create React App)
+    
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+const foods = ['pizza', 'pasta', 'spaghetti', 'ramen', 'eggs bennedict', 'dumplings', 'noodles', 'pork bun'];
+
+function App(){
+  return (
+    <div className='App'>
+      <h1>Our Menu!</h1>
+      <ul>
+        {
+          foods.map(food => {
+            return <li>{food}</li>
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+
+export default App;
